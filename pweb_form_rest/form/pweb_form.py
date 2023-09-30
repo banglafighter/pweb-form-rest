b92fdbba25e0a55c39f9e29aa92568db36029a3e
hmtmcse <hmtm.cse@gmail.com>
2023-09-30 13:22:09 +0600

    Working On SSR

--- pweb_form_rest/form/pweb_form.py
+++ pweb_form_rest/form/pweb_form.py
@@ -13,3 +13,9 @@ class PWebForm(PWebBaseForm, PWebRestDTO):
         super().__init__(*args, **kwargs)
         self.definition = FormDefinition()
         self.definition.init(self.declared_fields)
+
+    def set_select_option(self, field_name, select_options: dict):
+        return self.definition.set_select_option(field_name=field_name, select_options=select_options)
+
+    def process_and_set_option(self, field_name, options: list, key_name: str, value_name: str):
+        return self.definition.process_and_set_option(field_name=field_name, options=options, key_name=key_name, value_name=value_name)
--- pweb_form_rest/form/pweb_form_definition.py
+++ pweb_form_rest/form/pweb_form_definition.py
@@ -21,3 +21,22 @@ class FormDefinition:
             form_field = getattr(existing_form_definition, declared_field_definition.name)
         form_field = self.process_form_filed.start(declared_field_definition, form_field=form_field)
         return form_field
+
+    def get_form_field(self, field_name):
+        if hasattr(self, field_name):
+            return getattr(self, field_name)
+        return None
+
+    def set_select_option(self, field_name, select_options: dict):
+        form_field = self.get_form_field(field_name=field_name)
+        if not form_field or not select_options or not isinstance(select_options, dict):
+            return False
+        form_field.selectOptions = select_options
+        return True
+
+    def process_and_set_option(self, field_name, options: list, key_name: str, value_name: str):
+        select_options = {}
+        for option in options:
+            if key_name in option and value_name in option:
+                select_options[option[key_name]] = option[value_name]
+        return self.set_select_option(field_name=field_name, select_options=select_options)
