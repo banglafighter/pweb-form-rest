d6555e4a3629f423152fb4e8f916506fd00fec7c
hmtmcse <hmtm.cse@gmail.com>
2023-10-02 11:19:39 +0600

    generalize ui helper

--- pweb_form_rest/ui/pweb_ui_helper.py
+++ pweb_form_rest/ui/pweb_ui_helper.py
@@ -10,23 +10,12 @@ from pweb_form_rest.form.pweb_form import PWebForm
 class UIUtil:
     request_data: RequestData = RequestData()
 
-    def get_status_message(self):
+    @property
+    def pweb_flashed_messages(self):
         messages = get_flashed_messages(with_categories=True)
         response = {}
-        message_stack = ""
-        for status, message in messages:
-            if not status or not message:
-                continue
-            if status == "success":
-                response["isSuccess"] = True
-            elif "isSuccess" not in response:
-                response["isSuccess"] = False
-
-            if message:
-                message_stack += message + " "
-
-        if response and message_stack:
-            response["message"] = message_stack.strip()
+        for key, value in messages:
+            response[key] = value
         return json.dumps(response)
 
     def base_url(self):
@@ -40,10 +29,6 @@ class UIUtil:
         </script>
         """
         return script
-
-    @property
-    def search_value(self):
-        return self.request_data.get_query_args_value("search", "")
 
 
 class PWebSSRUIHelper:
