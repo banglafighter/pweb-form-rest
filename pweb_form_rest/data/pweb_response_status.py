f10b4da4b7241974b2e45cf0841fed37c32d3298
hmtmcse <hmtm.cse@gmail.com>
2023-10-10 17:52:38 +0600

    Completed API Login system

--- pweb_form_rest/data/pweb_response_status.py
+++ pweb_form_rest/data/pweb_response_status.py
@@ -11,6 +11,10 @@ class PWebResponseCode:
     error = 5100
     validation_error = 5101
 
+    invalid_token_code = 5500
+    token_expired_code = 5501
+    token_error_code = 5502
+
 
 class PWebHTTPCode:
     # SUCCESS Codes
