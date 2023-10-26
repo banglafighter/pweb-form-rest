54218ff3951e5e4d4b46e1aa6aa4ab4400e45a36
hmtmcse <hmtm.cse@gmail.com>
2023-10-26 20:45:53 +0600

    open the method with rename for more use

--- pweb_form_rest/crud/pweb_file_data_crud.py
+++ pweb_form_rest/crud/pweb_file_data_crud.py
@@ -23,7 +23,7 @@ class FileDataCRUD(PWebCRUDCommon):
         model.save()
         return model
 
-    def _process_data_and_file_upload(self, request_dto: PWebDataDTO, upload_path, override_names: dict = None, existing_model=None, form_data: dict = None):
+    def get_model_by_upload_file_data(self, request_dto: PWebDataDTO, upload_path, override_names: dict = None, existing_model=None, form_data: dict = None):
         if not form_data:
             form_data = self.pweb_crud.get_form_data(request_dto)
         model = self.pweb_crud.populate_model(form_data, request_dto, instance=existing_model)
@@ -32,7 +32,7 @@ class FileDataCRUD(PWebCRUDCommon):
         return self.upload_and_save_file(form_data=form_data, model=model, request_dto=request_dto, upload_path=upload_path, override_names=override_names)
 
     def upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = "Successfully created!", form_data=None):
-        model = self._process_data_and_file_upload(request_dto=request_dto, upload_path=upload_path, override_names=override_names, form_data=form_data)
+        model = self.get_model_by_upload_file_data(request_dto=request_dto, upload_path=upload_path, override_names=override_names, form_data=form_data)
         return self.message_or_data_response(model=model, response_dto=response_dto, response_message=response_message)
 
     def update_upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = "Successfully updated!", existing_model=None, form_data: dict = None, query=None):
@@ -42,7 +42,7 @@ class FileDataCRUD(PWebCRUDCommon):
         if not existing_model:
             existing_model = self.get_by_id(form_data['id'], query=query, exception=True)
 
-        model = self._process_data_and_file_upload(request_dto=request_dto, upload_path=upload_path, override_names=override_names, form_data=form_data, existing_model=existing_model)
+        model = self.get_model_by_upload_file_data(request_dto=request_dto, upload_path=upload_path, override_names=override_names, form_data=form_data, existing_model=existing_model)
         return self.message_or_data_response(model=model, response_dto=response_dto, response_message=response_message)
 
     def delete_file(self, upload_path, filename, response_message: str = "Successfully deleted!"):
