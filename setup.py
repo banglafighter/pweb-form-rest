a49be692521c4651ec1f505329f8d50f4c0ef25f
hmtmcse <hmtm.cse@gmail.com>
2023-11-08 08:21:20 +0600

    release unlisted version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_dependencies():
 
 setup(
     name='pweb-form-rest',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/banglafighter/pweb-form-rest',
     license='Apache 2.0',
     author='Bangla Fighter',
