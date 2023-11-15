b99db83fc3508148f45e2c23848f6b67d3b728bd
hmtmcse <hmtm.cse@gmail.com>
2023-11-15 09:55:48 +0600

    Split message into seperate class

--- pweb_form_rest/crud/pweb_rest_data_crud.py
+++ pweb_form_rest/crud/pweb_rest_data_crud.py
@@ -4,6 +4,12 @@ from pweb_form_rest.crud.pweb_file_data_crud import FileDataCRUD
 from pweb_orm import PWebBaseModel
 
 
+class RESTMessage:
+    create_success = "Successfully created!"
+    delete_success = "Successfully deleted!"
+    update_success = "Successfully updated!"
+
+
 class RESTDataCRUD(PWebCRUDCommon):
     file_data_crud = None
 
@@ -21,7 +27,7 @@ class RESTDataCRUD(PWebCRUDCommon):
             data = self.get_json_data(request_dto)
         return self.save(data=data, request_dto=request_dto)
 
-    def create(self, request_dto: PWebDataDTO, response_dto: PWebDataDTO = None, response_message: str = "Successfully created!", data: dict = None):
+    def create(self, request_dto: PWebDataDTO, response_dto: PWebDataDTO = None, response_message: str = RESTMessage.create_success, data: dict = None):
         model = self.create_and_get_model(data=data, request_dto=request_dto)
         return self.message_or_data_response(model, response_dto, response_message)
 
@@ -29,15 +35,15 @@ class RESTDataCRUD(PWebCRUDCommon):
         existing_model = self.get_by_id(model_id, exception=True, query=query)
         return self.response_maker.data_response(existing_model, response_dto)
 
-    def update(self, request_dto: PWebDataDTO, response_dto: PWebDataDTO = None, response_message: str = "Successfully updated!", existing_model=None, data: dict = None, query=None):
+    def update(self, request_dto: PWebDataDTO, response_dto: PWebDataDTO = None, response_message: str = RESTMessage.update_success, existing_model=None, data: dict = None, query=None):
         model = self.update_and_get_model(request_dto=request_dto, existing_model=existing_model, query=query, data=data)
         return self.message_or_data_response(model, response_dto, response_message)
 
-    def delete(self, model_id: int, response_message: str = "Successfully deleted!", query=None):
+    def delete(self, model_id: int, response_message: str = RESTMessage.delete_success, query=None):
         self.soft_remove(model_id=model_id, query=query)
         return self.response_maker.success_message(response_message)
 
-    def hard_delete(self, model_id: int, response_message: str = "Successfully deleted!", query=None):
+    def hard_delete(self, model_id: int, response_message: str = RESTMessage.delete_success, query=None):
         self.remove(model_id, query=query)
         return self.response_maker.success_message(response_message)
 
@@ -49,8 +55,8 @@ class RESTDataCRUD(PWebCRUDCommon):
         data_list = self.read_all(query=query, search_fields=search_fields, sort_field=sort_field, sort_order=sort_order, enable_pagination=False)
         return self.response_maker.list_data_type_response(data_list, response_dto=response_dto)
 
-    def upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = "Successfully created!", form_data=None):
+    def upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = RESTMessage.create_success, form_data=None):
         return self.file_data_crud.upload_file_data(request_dto=request_dto, upload_path=upload_path, response_dto=response_dto, override_names=override_names, response_message=response_message, form_data=form_data)
 
-    def update_upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = "Successfully updated!", existing_model=None, form_data: dict = None, query=None):
+    def update_upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = RESTMessage.update_success, existing_model=None, form_data: dict = None, query=None):
         return self.file_data_crud.update_upload_file_data(request_dto=request_dto, upload_path=upload_path, response_dto=response_dto, override_names=override_names, response_message=response_message, existing_model=existing_model, form_data=form_data, query=query)
